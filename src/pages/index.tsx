adaf7a381959ed9f29e2e7d8773b265ddeec1189
JustinLung <justinlung77@gmail.com>
2022-10-27 12:26:34 +0200

    added @ts-ignore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { GetServerSideProps } from "next"
-import { PokemonCard } from '../components/PokemonCard/PokemonCard'
-import { Header } from '../components/Header/Header'
-import styles from './styles/index.module.css'
-import Link from 'next/link'
-import { ScrollToTop } from "../components/ScrollToTop/ScrollToTop"
+import { GetServerSideProps } from "next";
+import { PokemonCard } from "../components/PokemonCard/PokemonCard";
+import { Header } from "../components/Header/Header";
+import styles from "./styles/index.module.css";
+import Link from "next/link";
+import { ScrollToTop } from "../components/ScrollToTop/ScrollToTop";
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 type Pokemons = {
-  pokemons: any
-}
+  pokemons: string;
+  i: number;
+};
 
 function Home({ pokemons }: Pokemons) {
   const [showButton, setShowButton] = useState(false);
@@ -25,38 +26,48 @@ function Home({ pokemons }: Pokemons) {
   }, []);
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <Header />
-      <section className={styles['pokemon-list']}>
-        {pokemons.results.map((pokemon, i) => {
-          const pokemonId = i + 1
-          return (
-            <Link href={`/pokemon/${pokemonId}`} key={pokemon?.id}>
-              <a rel="noopener">
-                <PokemonCard pokemonId={pokemonId} pokemonName={pokemon?.name} pokemonImage={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`}></PokemonCard>
-              </a>
-            </Link>
-          )
-        })}
+
+      <section className={styles["pokemon-list"]}>
+        {
+          // @ts-ignore
+          pokemons.results.map((pokemon, i) => {
+            const pokemonId = i + 1;
+            return (
+              <Link href={`/pokemon/${pokemonId}`} key={pokemon?.id}>
+                <a rel="noopener">
+                  <PokemonCard
+                    pokemonId={pokemonId}
+                    pokemonName={pokemon?.name}
+                    pokemonImage={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`}
+                  ></PokemonCard>
+                </a>
+              </Link>
+            );
+          })
+        }
       </section>
-      {showButton && (
-        <ScrollToTop />
-      )}
+      {showButton && <ScrollToTop />}
     </motion.div>
-  )
+  );
 }
 
 export async function getServerSideProps(context: GetServerSideProps) {
-  const baseURL = 'https://pokeapi.co/api/v2'
-  const pokemonListEndpoint = 'pokemon/?limit=898'
+  const baseURL = "https://pokeapi.co/api/v2";
+  const pokemonListEndpoint = "pokemon/?limit=898";
 
-  const res = await fetch(`${baseURL}/${pokemonListEndpoint}`)
-  const data = await res.json()
+  const res = await fetch(`${baseURL}/${pokemonListEndpoint}`);
+  const data = await res.json();
   return {
     props: {
-      pokemons: data
-    }
-  }
+      pokemons: data,
+    },
+  };
 }
 
-export default Home
+export default Home;
