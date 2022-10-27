49b58a1e3cefe609f8d816b4ccd75dac3076c338
JustinLung <justinlung77@gmail.com>
2022-10-27 12:22:31 +0200

    added ts-ignore

--- src/components/SearchPokemon/SearchPokemon.tsx
+++ src/components/SearchPokemon/SearchPokemon.tsx
@@ -1,25 +1,29 @@
-import styles from './SearchPokemon.module.css'
-import React, { useRef } from 'react';
-
+import styles from "./SearchPokemon.module.css";
+import React, { useRef } from "react";
 
 export function SearchPokemon() {
-  const searchInput = useRef(null)
+  const searchInput = useRef(null);
 
   function submitSearch(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    history.pushState({}, '', `/pokemon/${searchInput.current?.value}`)
+    e.preventDefault();
+    // @ts-ignore
+    history.pushState({}, "", `/pokemon/${searchInput.current?.value}`);
     location.reload();
   }
 
   return (
     <form className={styles.search} onSubmit={submitSearch}>
-      <input type="text" name="search_pokemon"
-        className={styles['search__input']}
+      <input
+        type="text"
+        name="search_pokemon"
+        className={styles["search__input"]}
         placeholder="✨ Search Pokemon..."
         autoComplete="off"
         ref={searchInput}
       />
-      <button className={styles['search__button']}><img src="assets/icons/search.svg" alt="Search Icon" /></button>
+      <button className={styles["search__button"]}>
+        <img src="assets/icons/search.svg" alt="Search Icon" />
+      </button>
     </form>
-  )
+  );
 }
